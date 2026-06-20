6b99e301100d03610a7a65c463aa479b4b419219
armin ahari <ahariarmin@gmail.com>
2026-06-21 00:33:33 +0330

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    hostname = socket.gethostname()
+    return f"Hello from Kubernetes Pod: {hostname}"
+
+app.run(host="0.0.0.0", port=5000)
